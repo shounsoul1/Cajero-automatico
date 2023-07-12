93a31c9e0396f048bd523f866df90e9dd3b28114
CamiloR <camilorodriguez3148@gmail.com>
2023-07-11 22:43:53 -0500

    Cambios al cajero:)

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 console.log("Hola mundo!!")
 let cuentas = [
-    {nombre: ""},
     {nombre: "Natalia", saldo: 200, clave: 12345},
-    {nombre: "Carlos", saldo: 290, clave: 223344},
-    {nombre: "Joaquin", saldo: 67, clave: 11111}
+    {nombre: "Carlos", saldo: 990, clave: 223344},
+    {nombre: "Joaquin", saldo: 10, clave: 11111}
 ]
+let indice = document.getElementById("persona").value;
+let nombre = cuentas[indice].nombre;
+let interfaz = "content";
+
 document.getElementById("persona").addEventListener("change", leerPersona)
 document.getElementById("acceder").addEventListener("click", validarClave);
+document.getElementById("consultar").addEventListener("click", consultarSaldo);
+document.getElementById("ingresar").addEventListener("click", ingresarMonto);
+document.getElementById("retirar").addEventListener("click", retirarMonto);
+
+document.getElementById("regresar").addEventListener("click", regresar);
+document.getElementById("salir").addEventListener("click", salir); 
+document.getElementById("regresar2").addEventListener("click", regresar);
+document.getElementById("salir2").addEventListener("click", salir); 
+document.getElementById("regresar3").addEventListener("click", regresar);
+document.getElementById("salir3").addEventListener("click", salir); 
+
+
+
+
 function leerPersona(){
-    let indice = document.getElementById("persona").value;
-    let nombre = cuentas[indice].nombre;
-    let saldo = cuentas[indice].saldo; // aun no le doy un uso
-    document.getElementById("saludo").innerHTML = "Hola, " + nombre;
-    if (nombre === ""){
-        document.getElementById("saludo").innerHTML = "";
-    }
+    indice = document.getElementById("persona").value;
+    nombre = cuentas[indice].nombre;
 }
+leerPersona()
 function validarClave(){
     let indice = document.getElementById("persona").value;
     let nombre = cuentas[indice].nombre;
@@ -31,17 +44,67 @@ function validarClave(){
     if (usuarioValido){
         document.querySelector(".input").style.display = "none"
         document.querySelector("#interfaz-content").style.display = "block";
+        document.getElementById("bienvenida").innerHTML = `Bievenid@ ${nombre}`;
     } else if (document.getElementById("clave").value  == "") {
-        alert("Introduce una clave")
+        document.getElementById("error").innerHTML = "Introduce una clave"
+        document.getElementById("error").style.display = "block"
     }
         else{
         document.getElementById("error").innerHTML = "La clave introducida es incorrecta.";
         document.getElementById("error").style.display = "block"
-        
-        setTimeout(function() {
-            document.getElementById("error").style.display = "none";
-        }, 3000);
+    } 
+}
+    function consultarSaldo(){
+        let saldo = cuentas[indice].saldo;   
+        // para acceder a las interfaces
+        document.getElementById("interfaz-consulta").style.display = "block"
+        document.getElementById("interfaz-content").style.display = "none"
+        // representar el saldo de cada usuario
+        document.getElementById("saldo").innerHTML = `Su saldo es: ${saldo}$`;
+            if (saldo <= 10){
+                setTimeout(function() {
+                    alert("¡Su saldo se está agotando!");
+                }, 500);
+            }   else if(saldo >= 990){
+                setTimeout(function() {
+                    alert("Le sugerimos que haga un retiro o transaccion");
+                }, 500);
+            }
+        interfaz = "consulta";       
+}   
+function regresar() {
+    if (interfaz === "consulta") {
+        document.getElementById("interfaz-content").style.display = "block";
+        document.getElementById("interfaz-consulta").style.display = "none";
+    } else if (interfaz === "ingreso") {
+        document.getElementById("interfaz-content").style.display = "block";
+        document.getElementById("interfaz-ingreso").style.display = "none";
+    } else if(interfaz === "retiro"){
+        document.getElementById("interfaz-content").style.display = "block";
+        document.getElementById("interfaz-retiro").style.display = "none";
     }
+}
+
+function salir() {
+    location.reload();
+}
+function ingresarMonto(){
+    console.log("algo")
+    document.getElementById("interfaz-ingreso").style.display = "block"
+    document.getElementById("interfaz-content").style.display = "none"
+    
+    interfaz = "ingreso";
+}
+function retirarMonto(){
+    console.log("algo")
+    saldo = cuentas[indice].saldo;
+    document.getElementById("interfaz-retiro").style.display = "block"
+    document.getElementById("interfaz-content").style.display = "none"
+    document.getElementById("saldo3").innerHTML = `Su saldo es: ${saldo}$`;
+    
+
+    
+    interfaz = "retiro";
 }
 
 
