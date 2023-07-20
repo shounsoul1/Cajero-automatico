2ffbb651aa8011d24b17a23813cb176599917a41
CamiloR <camilorodriguez3148@gmail.com>
2023-07-20 15:20:42 -0500

    Cambios finales

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Hola mundo!!")
 let cuentas = [
-    {nombre: "Natalia", saldo: 200, clave: 12345},
-    {nombre: "Carlos", saldo: 990, clave: 223344},
-    {nombre: "Joaquin", saldo: 10, clave: 11111}
+    {nombre: "Diana", saldo: 200, clave: 12345},
+    {nombre: "Alejo", saldo: 990, clave: 223344},
+    {nombre: "Sebas", saldo: 10, clave: 11111}
 ]
 let indice = document.getElementById("persona").value;
 let nombre = cuentas[indice].nombre;
@@ -12,8 +12,12 @@ let saldos = localStorage.getItem("saldos") ? JSON.parse(localStorage.getItem("s
 document.getElementById("persona").addEventListener("change", leerPersona)
 document.getElementById("acceder").addEventListener("click", validarClave);
 document.getElementById("consultar").addEventListener("click", consultarSaldo);
-document.getElementById("ingresar").addEventListener("click", ingresarMonto);
-document.getElementById("retirar").addEventListener("click", retirarMonto);
+document.getElementById("ingresar-monto").addEventListener("click", ingresarMonto);
+document.getElementById("realizar-ingreso").addEventListener("click", realizarIngreso);
+
+document.getElementById("retirar-monto").addEventListener("click", retirarMonto);
+document.getElementById("retirar").addEventListener("click", retiroMonto)
+
 
 document.getElementById("regresar").addEventListener("click", regresar);
 document.getElementById("salir").addEventListener("click", salir); 
@@ -72,49 +76,78 @@ function validarClave(){
         interfaz = "consulta";       
 }
 function ingresarMonto(){
-    let inputIngreso = document.getElementById("input-ingreso");
-    let ingresar = document.getElementById("ingresar-monto");
-    
-    ingresar.addEventListener("click", function () {
-        let inputValueIn = parseFloat(inputIngreso.value);
-        let saldo = saldos[nombre] || cuentas.find((cuenta) => cuenta.nombre === nombre).saldo || 0;
-        saldo += inputValueIn; 
-        saldos[nombre] = saldo;
-        document.getElementById("monto_pre-ingreso").innerHTML = `Su nuevo saldo es ${saldo}$`;
-
-        localStorage.setItem("saldos", JSON.stringify(saldos));
-    });
     document.getElementById("interfaz-ingreso").style.display = "block"
     document.getElementById("interfaz-content").style.display = "none"
     
     let saldo = saldos[nombre] || cuentas.find((cuenta) => cuenta.nombre === nombre).saldo || 0;
     document.getElementById("monto_pre-ingreso").innerHTML = `Su saldo es: ${saldo}$`;
+    if(saldo >= 990){
+        setTimeout(function() {
+            alert("No puede ingresar mas de 990");
+        }, 500);
+            document.getElementById("input-ingreso").type = "text";
+            document.getElementById("input-ingreso").style.color = "red";
+            document.getElementById("input-ingreso").value = "XXXXXXXXXX";
+            document.getElementById("input-ingreso").disabled = true;
+            document.getElementById("realizar-ingreso").style.cursor = "not-allowed";
+            document.getElementById("realizar-ingreso").ariaDisabled = true;
+            document.getElementById("realizar-ingreso").classList.add("disabled");
+    }  
+
     interfaz = "ingreso";
 }
 function retirarMonto(){
-    console.log("algo")
-    let inputRetiro = getElementById("input-ingreso");
-    let retiro = getElementById("retirar-monto");
-
-    retiro.addEventListener("click", function(){
-        let inputValueRe = parseFloat(inputRetiro.value);
-        let saldo = saldos[nombre] || cuentas.find((cuenta) => cuenta.nombre === nombre).saldo || 0;
-        saldo -= inputValueRe;
-        saldos[nombre] = saldo;
-        document.getElementById("monto_pre-ingreso").innerHTML = `Su nuevo saldo es ${saldo}$`;
-        
-        localStorage.setItem("saldos", JSON.stringify(saldos));
-    })
+    
     document.getElementById("interfaz-retiro").style.display = "block"
     document.getElementById("interfaz-content").style.display = "none"
     
     let saldo = saldos[nombre] || cuentas.find((cuenta) => cuenta.nombre === nombre).saldo || 0;
-    
-    document.getElementById("saldo3").innerHTML = `Su saldo es: ${saldo}$`;
-    
-    interfaz = "retiro";
-}
 
+    if(saldo <= 10){
+        setTimeout(function() {
+            alert("No puede retirar por debajo de 10");
+        }, 500);
+            document.getElementById("input-retiro").type = "text";
+            document.getElementById("input-retiro").style.color = "red";
+            document.getElementById("input-retiro").value = "XXXXXXXXXX";
+            document.getElementById("input-retiro").disabled = true;
+            document.getElementById("retirar").style.cursor = "not-allowed";
+            document.getElementById("retirar").ariaDisabled = true;
+            document.getElementById("retirar").classList.add("disabled");
+    } 
+
+    document.getElementById(id="monto_pre-retiro").innerHTML = `Su saldo es: ${saldo}$`;
+    interfaz = "retiro";
+
+}
+    function realizarIngreso() {
+        let inputIngreso = document.getElementById("input-ingreso");
+        let inputValueIn = parseFloat(inputIngreso.value);
+        inputIngreso.value = '';
+        
+        let saldo = saldos[nombre] || cuentas.find((cuenta) => cuenta.nombre === nombre).saldo || 0;
+        
+        saldo += inputValueIn; 
+        saldos[nombre] = saldo;
+        document.getElementById("monto_pre-ingreso").innerHTML = `Su nuevo saldo es ${saldo}$`;
+
+        localStorage.setItem("saldos", JSON.stringify(saldos));
+    };
+    
+
+function retiroMonto(){
+        let inputRetiro = document.getElementById("input-retiro");
+        let inputValueRe = parseFloat(inputRetiro.value);
+        inputRetiro.value = '';    
+
+        let saldo = saldos[nombre] || cuentas.find((cuenta) => cuenta.nombre === nombre).saldo || 0;
+
+        saldo -= inputValueRe;
+        saldos[nombre] = saldo;
+        document.querySelector(".monto_post-retiro").innerHTML = `Su nuevo saldo es: ${saldo}$`;
+        
+        localStorage.setItem("saldos", JSON.stringify(saldos));
+    }
 function regresar() {
     if (interfaz === "consulta") {
         document.getElementById("interfaz-content").style.display = "block";
